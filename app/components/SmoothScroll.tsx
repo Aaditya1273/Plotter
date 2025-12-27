71743b7213a0c04d786670d71f4eee7d940fedc2
[Aaditya1273] <aadityaiitian1515@gmail.com>
2025-12-27 12:40:49 +0530

    Fix SmoothScroll component - remove lenis dependency

--- app/components/SmoothScroll.tsx
+++ app/components/SmoothScroll.tsx
@@ -1,12 +1,16 @@
 'use client'
 
-import { ReactLenis } from '@studio-freight/react-lenis'
 import { ReactNode } from 'react'
 
 export default function SmoothScroll({ children }: { children: ReactNode }) {
+    // Simple smooth scroll implementation without external dependencies
     return (
-        <ReactLenis root options={{ lerp: 0.1, duration: 1.5, smoothWheel: true }}>
+        <div style={{ 
+            scrollBehavior: 'smooth',
+            overflowY: 'auto',
+            height: '100%'
+        }}>
             {children}
-        </ReactLenis>
+        </div>
     )
 }
